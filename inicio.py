f2f332ac546c98b1f9ab45d5fceaddfa0c3fe742
SofiChamorro <mschamorry@eafit.edu.co>
2025-02-27 07:24:13 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -5,4 +5,5 @@ st.header("Devon Aoki")
 st.write("fast and furious")
 image= Image.open('Sukicar.jpg')
 st.image(image)
-
+texto= st.text_input('Escribe algo','Text')
+st.write('El texto escrito es','Texto')
