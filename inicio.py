13c322e48405235e5ada8b737d4dfc88fcec283a
SofiChamorro <mschamorry@eafit.edu.co>
2025-02-27 07:36:52 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -7,3 +7,23 @@ image= Image.open('Sukicar.jpg')
 st.image(image)
 texto= st.text_input('Escribe algo','Text')
 st.write('El texto escrito es','Text')
+st.subheader("Columnas")
+col1,col2=st.colums(2)
+with col1:
+  st.subheader("Primera columna")
+  st.write("Conoces este carro?")
+  resp= st.checkbox ("Si")
+  if resp:
+    st.write("Me encanta")
+with col2:
+  st.subheader("Segunda columna")
+  modo= st.radio("A que personaje conoces",('Toreto','Suki','Brian'))
+  if modo== 'Toreto':
+    st.write(´Es el personaje principal')
+  if modo== 'Suki':
+    st.write(´Hablaremos sobre ella')
+  if modo== 'Brian':
+    st.write(´Su nombre real es Paul')
+    
+    
+  
